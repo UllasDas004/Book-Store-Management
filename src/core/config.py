339b8bd256017ef8244479184aa71b84cd75b9b3
UllasDas004 <ullasdas110904@gmail.com>
2026-03-04 17:48:07 +0530

    authentication utilities are added

--- src/core/config.py
+++ src/core/config.py
@@ -9,6 +9,10 @@ class Settings(BaseSettings):
     POSTGRES_HOST: str
     POSTGRES_PORT: str
 
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    
     @computed_field
     @property
     def DATABASE_URL(self) -> PostgresDsn:
