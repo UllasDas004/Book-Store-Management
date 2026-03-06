539fdf8b1ff57b12ae39f1158c6a4b5ec85b3964
UllasDas004 <ullasdas110904@gmail.com>
2026-03-06 12:14:16 +0530

    user login access time increased

--- src/core/config.py
+++ src/core/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 7*24*60
     
     @model_validator(mode='after')
     def assemble_db_connection(self) -> 'Settings':
